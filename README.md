87353aa687e97f078a249506ef6787c33f29650c
13tinydots <13tinydots@users.noreply.github.com>
2021-05-26 18:33:01 -0500

    pseudo-code and css format

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 wireframe and layout complete. comments showing basic structure in progress am 05/26/2021 12:19pm
 
 basic semantic HTML complete.  grid container complete with responsive layout. am 05/26/2021 1:35pm
+
+basic pseudo code done. format css.  am 05/26/2021 6:31pm
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,37 @@
 
+
+
+
+
+//start an init
+//init hides all text
+// timer is reset
+// init gets high scores from memory
+// init shows welcome setup including start button - event listener
+// welcome message displayed
+// highscore option displayed - with event listener
+
+
+//set an event listener for the high score button
+
+//new function: high score storage and diplay - hide display button - include return
+
+//new function: setup an object literal that appends appropriate button code
+// -- for answers one through four that includes onclick listeners for each button
+
+//new function: setup control loop timer that includes a flag to be reset
+//-- if flag is tripped then on next interval the timer will be reduced by 5 seconds
+//-- if the timer === zero then the game is over - do not enter name for score.
+//-- if all questions are answered then the game is over and score is recorded
+// subroutine: calls random question picker and calls object literal function to display
+//  card class as buttons for response by event listeners
+
+
+//new function: random question picker that checks for if the question has already
+// -- been asked.
+
+//new card class: question: answer 1-4: includes flag for previously asked, correct answer.
+
+//use the class, not sure how to store, and recall - maybe use JSON?
+
+
